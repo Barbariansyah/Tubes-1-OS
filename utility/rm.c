5116917a390aaee7d12c9a53aea43e35e3c16a5c
Barbariansyah <43785141+Barbariansyah@users.noreply.github.com>
2019-04-01 17:39:04 +0700

    Add files via upload
    udah dicoba benerin rm nya

--- utility/rm.c
+++ utility/rm.c
@@ -8,65 +8,27 @@
 #define FALSE 0
 #define TRUE 1
 
-int isEqualPathName(char *path1, char *path2){
-   int i;
-   for(i = 0; i < MAX_DIRNAME; i++){
-      if (path1[i] != path2[i]){
-         return FALSE;
-      }else if(path1[i]=='\0' && path2[i]=='\0' && i!=0){
-         return TRUE;
-      }
-   }
-   return TRUE;
-}
-
 void main(){
-  char argv[512];
-  char dirs[SECTOR_SIZE];
-  char files[SECTOR_SIZE];
-  int i, j;
-  int isEqual;
+  char argv[SECTOR_SIZE];
   char parentIndex;
+  char fileName[SECTOR_SIZE];
+  char dirName[SECTOR_SIZE];
+  int resultDir;
+  int resultFile;
 
-  i = 0;
-  j = 0;
-  isEqual = 0;
+  interrupt(0x21, 0x21, &parentIndex, 0, 0);
+  interrupt(0x21, 0x22, argv, 0, 0);
+  copyString(fileName,argv,SECTOR_SIZE);
+  copyString(dirName,argv,SECTOR_SIZE);
 
-  interrupt(0x21,0x02, dirs, DIRS_SECTOR, 0);
-  interrupt(0x21,0x02, files, FILES_SECTOR, 0);
-  interrupt(0x21,0x21, parentIndex, 0, 0);
-  interrupt(0x21, 0x23, 0, argv, 0);
+  interrupt(0x21, (parentIndex << 8) | 0x09, fileName, &resultFile, 0);
+  interrupt(0x21, (parentIndex << 8) | 0x0A, dirName, &resultDir, 0);
 
-  while(i*DIRS_ENTRY_LENGTH < SECTOR_SIZE){
-    isEqual = isEqualPathName(argv, files + i*DIRS_ENTRY_LENGTH +1);
-
-    if (isEqual && files[i*DIRS_ENTRY_LENGTH] == parentIndex){
-         interrupt(0x21, 0x09, argv, 0, parentIndex);
-         break;
-    }
-    else{
-         i++;
-    }
+  if(resultDir == -1 && resultFile == -1){
+    interrupt(0x21, 0x00, "rm: cannot remove \'", 0, 0);
+    interrupt(0x21, 0x00, fileName, 0, 0);
+    interrupt(0x21, 0x00, "\': No such file or directory", 0, 0);
+    interrupt(0x21, 0x00, "\n\r", 0, 0);
   }
 
-  while(j*DIRS_ENTRY_LENGTH < SECTOR_SIZE){
-    isEqual = isEqualPathName(argv, dirs + j*DIRS_ENTRY_LENGTH +1);
-
-    if (isEqual && dirs[j*DIRS_ENTRY_LENGTH] == parentIndex){
-         interrupt(0x21, 0x0A, argv, 0, parentIndex);
-         break;
-    }
-    else{
-         j++;
-    }
-  }
-
-  if (i == MAX_FILES && j == MAX_DIRS){
-      interrupt(0x21, 0x00, "rm: cannot remove \'", 0, 0);
-      interrupt(0x21, 0x00, argv, 0, 0);
-      interrupt(0x21, 0x00, "\': No such file or directory", 0, 0);
-      interrupt(0x21, 0x00, "\n\r", 0, 0);
-      return;
-  }
-  interrupt(0x21, 0x07, 0, 0, 0);
 }
